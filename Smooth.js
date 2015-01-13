1c6f1f92eb15debd3261ed1239a7b978661f61ca
panxinwu <759822115@qq.com>
2015-01-13 15:45:07 +0800

    优化：3d加速

--- Smooth.js
+++ Smooth.js
@@ -15,112 +15,41 @@ function Smooth(element){
 		var 
 		func = func,
 		callback = callback,
-		moveX,
-		moveY,
 		startX,
 		startY,
-		currentX,
-		currentY,
-		lastX,
-		lastY,
-		changeX,
-		changeY,
-		moving = false,
-		firstMove = true;
+		resultX = 0,
+		resultY = 0,
+		data;
 
-		var X = 0,Y=0;
+			_this.dom.addEventListener('touchstart', function (e) {
+				startX = e.touches[0].clientX;
+				startY = e.touches[0].clientY;
+			},false);
 
 			_this.dom.addEventListener('touchmove', function (e) {
 				e.stopPropagation();
 				e.preventDefault();
-					moving = true;
-					if (firstMove) {
-						startX = e.changedTouches[0].clientX;
-						startY = e.changedTouches[0].clientY;
-						firstMove = false;
-						lastX = e.changedTouches[0].clientX;
-						lastY = e.changedTouches[0].clientY;	
-					} else {
+						var translate = dom.style.webkitTransform.match(/\-?[0-9]+/g);
+						console.log(translate);
+						resultX = e.touches[0].clientX -  startX + parseFloat(translate[1]);
+						resultY = e.touches[0].clientY - startY + parseFloat(translate[2]);
 
-						currentX = e.changedTouches[0].clientX;
-						currentY = e.changedTouches[0].clientY;
-
-						changeX = currentX - lastX;
-						changeY = currentY - lastY;
-
-						moveX = e.changedTouches[0].clientX - startX;
-						moveY = e.changedTouches[0].clientY - startY;
-
-
-						var data = {
-							moveX: moveX,
-							moveY: moveY,
-							changeX: changeX,
-							changeY: changeY
+						data = {
+							resultX: resultX,
+							resultY: resultY,
 						}
-
+						startX = e.touches[0].clientX;
+						startY = e.touches[0].clientY;
+						//console.log(data);
 						if(func instanceof Function) {
 							func(data,e);
 						}
-
-
-						lastX = e.changedTouches[0].clientX;
-						lastY = e.changedTouches[0].clientY;	
-					}
-
 			}, false);
 
 			_this.dom.addEventListener('touchend', function (e) {
-				if (moving === false) return;
-				firstMove = true;
-				if(callback instanceof Function){
-					callback(e);	
-				}
-				moving = false;
 			}, false);
 
 	}
-
-	_Smooth.prototype.slide = function (func,xMove,yMove) {
-		var _this = this;
-		var
-		func = func,
-		xMove = xMove | 0,
-		yMove = yMove | 0,
-		startX,
-		startY,
-		moveX,
-		moveY;
-
-		_this.dom.addEventListener('touchstart', function (e) {
-			e.preventDefault();
-			e.stopPropagation();
-
-			startX = e.changedTouches[0].clientX;
-			startY = e.changedTouches[0].clientY;				
-	
-				
-		}, false);
-	
-		_this.dom.addEventListener('touchend', function (e) {
-			e.stopPropagation();
-			e.preventDefault();
-			
-			moveX = e.changedTouches[0].clientX - startX;
-			moveY = e.changedTouches[0].clientY - startY;
-
-
-			if (moveX >= xMove && moveY >= yMove){
-				var data = {
-						moveX: moveX,
-						moveY: moveY
-					}
-				func(data,e); 
-			}
-		}, false);
-	};
-
-
 	var dom = document.querySelector(element);
 	var smooth = new _Smooth(dom);
 	return smooth;
